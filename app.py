471ba96fab26cc12a7724433529d4809435a6575
rogergermag <rg@rogergerm.ch>
2026-03-14 12:56:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ REIHENFOLGE:
 
 STRUKTUR:
 - Beginne mit:
-«Seine Vorgesetzten schreiben folgendes über Herrn {daten['nachname']}:»
+Seine Vorgesetzten schreiben folgendes über Herrn {daten['nachname']}:
 
 - Danach folgen auf der gleichen Zeile die ca. 10 kurze Aussagen.
 - Alle Aussagen hintereinander als Fliesstext.
